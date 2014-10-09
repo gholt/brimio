70a33b336511e37f645091acf801df29703276a2
Gregory Holt <z-github@brim.net>
2014-10-08 23:53:00 -0500

    Fixed off by 1 error

--- scrambled.go
+++ scrambled.go
@@ -20,7 +20,7 @@ func NewSeededScrambled(seed int64) *Scrambled {
 func (s *Scrambled) Read(bs []byte) {
 	for i := len(bs) - 1; i >= 0; {
 		v := s.r.Int63()
-		for j := 7; i >= 0 && j >= 0; j-- {
+		for j := 6; i >= 0 && j >= 0; j-- {
 			bs[i] = byte(v)
 			i--
 			v >>= 8
